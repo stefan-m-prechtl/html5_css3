54573fcf53aa4c6ae7751c12e5a63ac080911685
Prechtl Stefan <Stefan.Prechtl@esg.de>
2019-08-09 14:05:15 +0200

    Erweiterung

--- lithtml/app.js
+++ lithtml/app.js
@@ -1,28 +1,89 @@
+"use strict";
+
 // Import lit-html
 //import {html, render} from 'https://unpkg.com/lit-html?module';
 //import { html, render } from './node_modules/lit-html/lit-html.js';
 import { html, render } from 'lit-html';
+import { repeat } from 'lit-html/directives/repeat.js';
 
-/*
-// Define a template
-let myTemplate = (data) => html`
-  <h1>${data.title}</h1>
-  <p>${data.content}</p>`;
+class Task {
+    constructor(id, content) {
+        this.id = id;
+        this.content = content;
+    }
+}
 
 
-// Render the template to the document
-const result = myTemplate({title: 'lit-hml', content: 'Hello from lit-html'});
-render(result, document.body);
-*/
+let todos = [];
+let counter = 0;
 
-const todos = [];
-todos.push('Einkaufen');
-todos.push('Aufräumen');
-todos.push('Gießen');
+const btnClearCaption = 'Clear';
 
-let myTemplate = (items) => html `
-  ${items.map((item) => html`<li>${item}</li>`)}
+const handlerBtnClear = {
+    handleEvent(e) {
+        todos = [];
+        counter = 0;
+        render(contentTemplate(todos), document.querySelector('#todocontent'));
+    }
+}
+
+const handlerBtnLoad = {
+    handleEvent(e) {
+        todos = loadData(5);
+        render(contentTemplate(todos), document.querySelector('#todocontent'));
+    }
+}
+
+const handlerBtnAdd = {
+    handleEvent(e) {
+        let task = new Task(++counter, `Aufgabe ${counter}`);
+        todos.push(task);
+        render(contentTemplate(todos), document.querySelector('#todocontent'));
+    }
+}
+
+const handlerBtnReverse = {
+    handleEvent(e) {
+        todos = todos.reverse();
+        render(contentTemplate(todos), document.querySelector('#todocontent'));
+    }
+}
+
+let buttonTemplate = html `
+  <button @click=${handlerBtnClear}>${btnClearCaption}</button>
+  <button @click=${handlerBtnLoad}>Load</button>
+  <button @click=${handlerBtnAdd}>Add</button>
+  <button @click=${handlerBtnReverse}>Reverse</button>
+  `;
+
+let contentTemplate = (items) => html `
+  ${items.map((item) => html`<li>${item.content}</li>`)}
 `;
 
+let contentTemplate2 = (items) => html `
+  ${repeat(items, (item) => item.id, (item,index) => html`
+    <li>${item.content}</li>
+  `)}
+`;
 
-render(myTemplate(todos), document.querySelector('#todoliste'));
+let completeTemplate = (items) => html `
+  ${buttonTemplate}
+  <ul id='todocontent'>${contentTemplate(items)}</ul>  
+  `;
+
+function loadData(quantity) {
+    counter = 0;
+    let result = [];
+    for (let index = 0; index < quantity; index++) {
+        result.push(new Task(++counter, `Aufgabe ${counter}`));
+    }
+    return result;
+}
+
+function init() {
+    todos = loadData(1000);
+    render(completeTemplate(todos), document.querySelector('#todoliste'));
+
+}
+
+init();
