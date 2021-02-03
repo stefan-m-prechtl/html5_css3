f9071bfc4263fd1e51e778c95f42b56d347f6f23
Stefan M Prechtl <stefanmprechtl@web.de>
2021-02-03 18:26:18 +0100

    Aktualisierung

--- Apps/todoMVP/js/util.js
+++ Apps/todoMVP/js/util.js
@@ -1,19 +1,21 @@
-// Selektion jQuery-like
+// ES6 Modules are always executed in strict mode, so there is no need for ‘use strict’
+
+// JQuery like Selektoren
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-// Eventhandling jQuery-like
-Node.prototype.on = function(name, fn) {
-    this.addEventListener(name, fn);
-    return this;
+// JQuery like Eventhandler
+Node.prototype.on = function (name, fn) {
+  this.addEventListener(name, fn);
+  return this;
+};
+NodeList.prototype.on = NodeList.prototype.on = function (name, fn) {
+  this.forEach((elem) => elem.on(name, fn));
+  return this;
 };
 
-NodeList.prototype.on = NodeList.prototype.addEventListener = function(name, fn) {
-    this.forEach(elem => elem.on(name, fn));
-    return this;
-};
-
-// Eigenschaften von Array (z.B. forEach) an NodeList "vererben"
+// Array-Methoden bereitstellen
 NodeList.prototype.__proto__ = Array.prototype;
+HTMLCollection.prototype.__proto__ = Array.prototype;
 
 export { $, $$ };
